2ec494512554bfc263927c547b3cec16c12846b3
Michel Silva <michelalmeidasilva@outlook.pt>
2022-11-04 15:42:39 -0300

    wip: added a Address class

---
+++ src/main/kotlin/br/com/bytebank/user/Address.kt
@@ -0,0 +1,4 @@
+package br.com.bytebank.user
+
+class Address(var number: String = "", var address: String = "", var city: String = "", var state: String = "", var country: String= "", var district: String = "" ) {
+}

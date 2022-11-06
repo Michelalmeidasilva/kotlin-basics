4711713d3b76550e9dfcec749ccda9f561db47c1
Michel Silva <michelalmeidasilva@outlook.pt>
2022-11-05 22:53:57 -0300

    wip: added a objects and data class examples

--- annotations.md
+++ annotations.md
@@ -1,5 +1,29 @@
 ### Pontos que necessitam ser reforçados
 
 1- Companion object
+
+    De acordo com o a documentação do Kotlin, as declarações de objeto ou object declarations criam um Singleton de maneira fácil.
+
 2- Object expression
+
+    Object expressions create objects of anonymous classes, that is, classes that aren't explicitly declared with the class declaration. Such classes are useful for one-time use. You can define them from scratch, inherit from existing classes, or implement interfaces. Instances of anonymous classes are also called anonymous objects because they are defined by an expression, not a name.
+
+
 3- Object declaration
+
+    The Singleton pattern can be useful in several cases, and Kotlin makes it easy to declare singletons:
+
+4 - Equals and Hashcode differences
+
+5 - Data class
+    
+    É uma classe para manter os dados, não é possivel ter comportamentos, apenas atributos
+    Implementa os métodos de equals, hashCode e toString(), as validações desses metodos serão baseados nas properties
+
+    O construtor precisa ter sempre as properties, nào é possivel passar as variaveis   
+
+
+
+--- 
+### Kotlin  
+1. 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,13 @@
+import br.com.bytebank.bank.Bank
 import br.com.bytebank.user.Account
+import br.com.bytebank.user.Address
 import br.com.bytebank.user.Client
 
 
 
 // object declarator ( singleton )
+// valor estático e global
+
 object ContadorSingleton {
     var totalDeContas = 0
 
@@ -14,7 +18,7 @@ object ContadorSingleton {
 
 
 class AccountTest {
-    // companion object
+    // companion object valor estático
     // se torna membro da classe
     companion object ContadorAcessible {
         var totalDeContas = 0
@@ -25,7 +29,9 @@ class AccountTest {
     }
 }
 
+data class Person(val cpf: String, val name: String, val test: Address) {
 
+}
 
 fun main() {
 
@@ -37,16 +43,31 @@ fun main() {
         }
     }
 
-    ContadorSingleton.totalDeContas = 10
-    ContadorSingleton.incremment()
-    println(ContadorSingleton.totalDeContas);
+    val michel = Person("04053333", "Michel Silva",  Address("300", "Rua gomercino ferreira")  )
+    val michel2 = Person("04053333", "Michel Silva",  Address("300", "Rua gomercino ferreira")  )
 
-    println(AccountTest.totalDeContas)
-    println(fran.getTest())
+    if(michel == michel2){
+        println(michel);
+    }
 
-    val test1 = Account(Client("michel1", "test1", "test1"), 8000);
-    val test2 = Account(Client("michel2", "test2", "test2"), 10000);
-    val test3 = Account(Client("michel3", "test3", "test3"), 12000);
-    val test4 = Account(Client("michel4", "test4", "test4"), 14000);
-    val test5 = Account(Client("michel5", "test5", "test5"), 16000);
+//
+//    ContadorSingleton.totalDeContas = 10
+//    ContadorSingleton.incremment()
+//    println(ContadorSingleton.totalDeContas);
+//
+//    println(AccountTest.totalDeContas)
+//    println(fran.getTest())
+//
+//    val test1 = Account(Client("michel1", "test1", "test1"), 8000);
+//    val test2 = Account(Client("michel2", "test2", "test2"), 10000);
+//    val test3 = Account(Client("michel3", "test3", "test3"), 12000);
+//    val test4 = Account(Client("michel4", "test4", "test4"), 14000);
+//    val test5 = Account(Client("michel5", "test5", "test5"), 16000);
+//
+//    customPrintln(Any())
+}
+
+
+fun customPrintln(value: Any?){
+    println(value)
 }
